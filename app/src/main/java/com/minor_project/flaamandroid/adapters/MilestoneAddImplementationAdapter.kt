f56a71ae0d389872f10b251e54c41f1fe1025a21
Soumya Jain <soumyajaincarmel@gmail.com>
2021-11-08 15:03:23 +0530

    .

---
+++ app/src/main/java/com/minor_project/flaamandroid/adapters/MilestoneAddImplementationAdapter.kt
@@ -0,0 +1,52 @@
+package com.minor_project.flaamandroid.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.CheckedTextView
+import android.widget.TextView
+import com.minor_project.flaamandroid.R
+
+class MilestoneAddImplementationAdapter(private val dataSet: ArrayList<String>, mContext: Context) :
+    ArrayAdapter<String>(mContext, R.layout.item_milestone_add_implementation, dataSet) {
+    private class ViewHolder {
+        lateinit var tv: TextView
+    }
+
+    override fun getCount(): Int {
+        return dataSet.size
+    }
+
+    override fun getItem(position: Int): String {
+        return dataSet[position]
+    }
+
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        var convertView = convertView
+        val viewHolder: ViewHolder
+        val result: View
+        if (convertView == null) {
+            viewHolder = ViewHolder()
+            convertView =
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_milestone_add_implementation, parent, false)
+            viewHolder.tv =
+                convertView.findViewById(R.id.tv_milestone_item_milestone_add_implementation)
+
+            result = convertView
+            convertView.tag = viewHolder
+        } else {
+            viewHolder = convertView.tag as ViewHolder
+            result = convertView
+        }
+        val item: String = getItem(position)
+        viewHolder.tv.text = item
+        return result
+    }
+}
